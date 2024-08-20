b91a07ad025b9b7d209662819d7f536eb0e7915a
jsmld <samvanerpersroijaards@gmail.com>
2024-08-20 17:25:25 -0300

    bullet time bby

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
   pygame.init()
@@ -14,8 +15,10 @@ def main():
   updateable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   asteroids = pygame.sprite.Group()
+  shots = pygame.sprite.Group()
 
   Player.containers = (updateable, drawable)
+  Shot.containers = (shots, updateable, drawable)
   Asteroid.containers = (asteroids, updateable, drawable)
   AsteroidField.containers = updateable
 
